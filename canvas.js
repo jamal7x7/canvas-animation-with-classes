1101cfe77dbabf9195eaa655b8f950469891a7e1
jamal7X7 <jamal7x7@gmail.com>
2019-01-06 21:11:59 +0000

    colors  changed

--- canvas.js
+++ canvas.js
@@ -8,7 +8,7 @@ const c = canvas.getContext('2d')
 const g ="#00FF00" 
 const b ="#00fff0" 
 
-const colorArray = [g, b, 'tomato', 'purple', 'yellow', 'pink']
+const colorArray = ['#C86DD7', '#877AFF',"#61DAFB", '#123', '#234', '#224', '#223']
  
 
 
@@ -18,14 +18,17 @@ const mouse = {
 }
 
 const maxRadius = 50
-const minRadius = 15
+
 
 addEventListener('mousemove', e => {
     mouse.x = e.clientX
     mouse.y = e.clientY
-
     console.log(mouse)
-
+})
+addEventListener('resize', e => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight 
+    init()
 })
 
 class Circle {
@@ -36,7 +39,11 @@ class Circle {
         this.dx = dx
         this.dy = dy
         this.color = color
+        this.minRadius = r
+        this.mindx = dx
+        this.mindy = dy
     }
+    
 
     info() {
         return `((Class::Circle) coordinate: ( ${this.x}, ${this.y})
@@ -62,15 +69,13 @@ class Circle {
         let d = Math.sqrt((mouse.x - this.x)**2 + (mouse.y - this.y)**2 )
        
         if ( d < 100)  {
-            let a = this.r
+            
             if(this.r < maxRadius) {
-                this.r += 10
+                this.r += 1
             }
 
-        } else {
-            if (this.r > minRadius) {
-                this.r -=10
-            } 
+        } else if (this.r > this.minRadius) {
+                this.r -= 1
         }
     
         this.x += this.dx
@@ -80,14 +85,18 @@ class Circle {
 }
 
 let circleArray = []
-for (let i=0; i < 200; i++) {
-    let r = 30 * Math.random()
-    let x = (innerWidth - 2*r) * Math.random() + r
-    let y = (innerHeight - 2*r) * Math.random() + r
-    let dx = 3 * Math.random()  
-    let dy = 3 * Math.random() 
-    let color = colorArray[Math.round(Math.random()*6 )]
-    circleArray = [...circleArray, new Circle(x,y,r,dx,dy, color)] 
+const init = () => {
+    circleArray = [] 
+    for (let i=0; i < 700; i++) {
+        let r = 5 * Math.random() + 1
+        let minRadius = r
+        let x = (innerWidth - 2*r) * Math.random() + r
+        let y = (innerHeight - 2*r) * Math.random() + r
+        let dx = 3 * Math.random()  
+        let dy = 3 * Math.random() 
+        let color = colorArray[Math.round(Math.random() * colorArray.length )]
+        circleArray = [...circleArray, new Circle(x,y,r,dx,dy, color)] 
+    }
 }
 
 const animate = () => {
@@ -96,4 +105,6 @@ const animate = () => {
 
     circleArray.map( item => item.update())
 }
+
+init()
 animate()
